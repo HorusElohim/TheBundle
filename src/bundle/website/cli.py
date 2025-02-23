869e6dc2f7ccb41c8351a70400d00d706750b062
HorusElohim <horuselohim@gmail.com>
2025-02-23 20:42:26 +0100

    ✨ unify CLI

--- src/bundle/website/cli.py
+++ src/bundle/website/cli.py
@@ -1,20 +1,22 @@
 import click
 import uvicorn
+from . import get_app
 
 
 @click.group()
-def cli():
+def website():
     """The Bundle CLI tool."""
     pass
 
 
-@cli.command()
+@website.command()
 @click.option("--host", default="127.0.0.1", help="Host to run the server on.")
 @click.option("--port", default=8000, type=int, help="Port to run the server on.")
 def start(host, port):
     """Start the FastAPI web server."""
-    uvicorn.run("bundle.website:app", host=host, port=port, reload=True, workers=4)
+    app = get_app()
+    uvicorn.run(app, host=host, port=port)
 
 
 if __name__ == "__main__":
-    cli()
+    website()
