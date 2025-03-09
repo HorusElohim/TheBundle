1d51b0f791185e6c7b656b86c1f79dabd627eaec
HorusElohim <horuselohim@gmail.com>
2025-03-09 10:01:46 +0100

    🧑‍💻 fix test CI - Add perf mode.

--- src/bundle/testing/cli.py
+++ src/bundle/testing/cli.py
@@ -1,8 +1,10 @@
+import os
+import asyncio
 import pytest
 import rich_click as click
 
 import bundle
-from bundle.core import data, logger, process, tracer
+from bundle.core import logger, tracer
 
 log = logger.get_logger(__name__)
 
@@ -19,48 +21,35 @@ async def python():
     pass
 
 
-class TestProcess(process.Process):
-    complete_logs: list[str] = data.Field(default_factory=list)
-
-    async def callback_stdout(self, line: str):
-        line = line.strip()
-        self.complete_logs.append(line)
-        if "PASSED" in line or "====" in line or "SKIPPED" in line:
-            log.info("%s", line)
-
-    async def callback_stderr(self, line: str):
-        line = line.strip()
-        self.complete_logs.append(line)
-
-
 @python.command("pytest")
 @tracer.Sync.decorator.call_raise
 @click.option("--show-exc", is_flag=True, help="Show expected trace Exceptions")
-async def pytest_cmd(show_exc: bool):
+@click.option("--perf", is_flag=True, help="Show expected trace Exceptions")
+async def pytest_cmd(show_exc: bool, perf: bool):
     """
     Run pytest directly from this CLI instance using pytest.main().
     This runs the tests in a separate thread.
     """
-    # Lower the logger level to show all messages during testing.
-    bundle.BUNDLE_LOGGER.setLevel(logger.Level.VERBOSE)
-
     # Avoid show tracer expected exception
     if not show_exc:
         bundle.core.tracer.DEFAULT_LOG_EXC_LEVEL = logger.Level.EXPECTED_EXCEPTION
+
+    # Avoid logger overhead
+    if perf:
+        os.environ["PERFORMANCE"] = "true"
 
     bundle_folder = bundle.Path(list(bundle.__path__)[0])
     tests_folder = bundle_folder.parent.parent / "tests"
     log.debug("bundle_folder=%s, tests_folder=%s", str(bundle_folder), tests_folder)
 
-    def run_pytest():
-        import nest_asyncio
+    # Run pytest.main() in a separate thread so that its event loop
+    # creation and teardown is isolated from the current (running) loop.
+    test_result = await asyncio.to_thread(pytest.main, [str(tests_folder)])
 
-        nest_asyncio.apply()
-        return tracer.Sync.call_raise(pytest.main, [str(tests_folder)])
-
-    run_pytest()
-
-    log.info("All tests passed successfully.")
+    if test_result == 0:
+        log.info("Test success")
+    else:
+        log.error("Test failed")
 
 
 testing.add_command(python)
