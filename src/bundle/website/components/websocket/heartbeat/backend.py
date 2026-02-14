34e1ca360596f340073c8a937837c3e8f2cfe528
HorusElohim <horuselohim@gmail.com>
2026-02-14 12:12:35 +0100

    ♻️ rework websockets components

--- src/bundle/website/components/websocket/heartbeat/backend.py
+++ src/bundle/website/components/websocket/heartbeat/backend.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
 from fastapi import APIRouter, WebSocket
 
 from ..base import keepalive_loop
 
 router = APIRouter()
 
-@router.websocket("/ws/ecc")
-async def ecc_websocket(websocket: WebSocket) -> None:
+
+@router.websocket("/ws/heartbeat")
+async def heartbeat_websocket(websocket: WebSocket) -> None:
     await keepalive_loop(websocket)
